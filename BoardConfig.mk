decf15bd1abc1b71d85a5cdfcb151eaa11d475e9
sksifat-4 <sksifat4445@gmail.com>
2026-09-21 19:43:12 +0000

    Violet:Switch to Nemesis Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
-TARGET_KERNEL_CONFIG := vendor/violet-perf_defconfig
+TARGET_KERNEL_CONFIG := nemesis_defconfig
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
